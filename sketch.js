0b5b8cff30d30628a2c6f8bff1e94083021259fd
norberto <otsukikienai@gmail.com>
2021-10-19 19:25:37 -0300

    atualizado'

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1, backgroundmg;
+var box1, pig1, backgroundmg, platform;
 
 function preload(){
  backgroundmg=loadImage("sprites/bg.png");
@@ -14,7 +14,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform=new Ground(150,305,300,170);
+
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -56,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    platform.display();
     bird.display();
 }
